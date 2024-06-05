e6e9c3b4e13e2aa95f789cb90f11a07aed2a1cb7
joeuni <dmsdl8917@gmail.com>
2024-06-05 13:13:20 +0900

    네비게이션 커스텀 훅

---
+++ src/hooks/useCustomMove.js
@@ -0,0 +1,45 @@
+import {
+  createSearchParams,
+  useNavigate,
+  useSearchParams,
+} from "react-router-dom";
+
+const getNum = (param, defaultValue) => {
+  //만약 param이 없으면, defaultValue로 리턴한다.
+  if (!param) {
+    return defaultValue;
+  }
+  return parseInt(param);
+};
+
+const useCustomMove = () => {
+  const navigate = useNavigate();
+
+  //파라미터 값
+  const [queryParams] = useSearchParams();
+
+  const page = getNum(queryParams.get("page"), 1);
+  const size = getNum(queryParams.get("size"), 10);
+
+  //page=3&size=10
+  const queryDefault = createSearchParams({ page, size }).toString(); //새로 추가
+
+  const moveToList = (pageParam) => {
+    let queryStr = "";
+
+    if (pageParam) {
+      //moveToList 함수에 파라미터 값이 있을 경우
+      const pageNum = getNum(pageParam.page, 1);
+      const sizeNum = getNum(pageParam.size, 10);
+      queryStr = createSearchParams({ page: pageNum, size: sizeNum }).toString;
+    } else {
+      //moveToList 함수에 파라미터 값이 있을 경우
+      queryStr = queryDefault;
+    }
+    navigate({ pathname: "../list", search: queryStr });
+  };
+
+  return { moveToList };
+};
+
+export default useCustomMove;
