a08c93e59ce0e193c136768391cab89b7435057c
joeuni <dmsdl8917@gmail.com>
2024-06-05 16:08:15 +0900

    확인 모달

---
+++ src/components/common/ConfirmModal.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function ConfirmModal({ message, onConfirm, onCancel }) {
+  return (
+    <div
+      className={`fixed top-0 left-0 z-[1055] flex h-full w-full justify-center  items-center  bg-black bg-opacity-20`}
+    >
+      <div className="absolute bg-white shadow opacity-100 w-1/4 rounded mt-10 mb-10 px-6 min-w-[600px]">
+        <div className="text-2xl  pt-4 pb-4 text-stone-700 ">{message}</div>
+        {/* button */}
+        <div className="justify-end flex">
+          <button
+            className="rounded bg-blue-500 mt-4 mb-4 px-6 pt-4 pb-4 text-lg hover:bg-blue-400 text-white"
+            onClick={() => {
+              if (onCancel) {
+                onCancel();
+              }
+            }}
+          >
+            취소
+          </button>
+          <button onClick={onConfirm}>확인</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ConfirmModal;
