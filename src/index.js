981a3ab74e9583cb48d30a03bffb58251c5e3ca7
joeuni <dmsdl8917@gmail.com>
2024-06-09 13:11:42 +0900

    리덕스 툴킷 설정 완료

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
