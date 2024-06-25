6e28f85dbc997a54c86efac44ea6b5ddabc633ea
joeuni <dmsdl8917@gmail.com>
2024-06-25 23:47:36 +0900

    선택한 카테고리 css 반영

--- src/components/menus/CategoryHeader.js
+++ src/components/menus/CategoryHeader.js
@@ -1,40 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function CategoryHeader() {
+  const [selectedMenu, setSelectedMenu] = useState(1);
   return (
     <div>
       {/* header menu */}
       <div className="w-full hidden lg:block border-b border-stone-200 h-16">
         <ul className="flex justify-center space-x-10 text-lg">
-          <li>
+          <li onClick={() => setSelectedMenu(1)}>
             <Link
               to={"/contents/genre/concert"}
-              className="hover:border-b-4 border-yellow-300 hover:font-semibold"
+              className={`${
+                selectedMenu === 1 && "font-bold border-b-4 border-yellow-300"
+              } hover:border-b-4 border-yellow-300 hover:font-semibold`}
             >
               콘서트
             </Link>
           </li>
-          <li>
+          <li onClick={() => setSelectedMenu(2)}>
             <Link
               to={"/contents/genre/musical"}
-              className="hover:border-b-4 border-yellow-300 hover:font-semibold"
+              className={`${
+                selectedMenu === 2 && "font-bold border-b-4 border-yellow-300"
+              } hover:border-b-4 border-yellow-300 hover:font-semibold`}
             >
               뮤지컬
             </Link>
           </li>
-          <li>
+          <li onClick={() => setSelectedMenu(3)}>
             <Link
               to={"/contents/genre/play"}
-              className="hover:border-b-4 border-yellow-300 hover:font-semibold"
+              className={`${
+                selectedMenu === 3 && "font-bold border-b-4 border-yellow-300"
+              } hover:border-b-4 border-yellow-300 hover:font-semibold`}
             >
               연극
             </Link>
           </li>
-          <li>
+          <li onClick={() => setSelectedMenu(4)}>
             <Link
               to={"/contents/genre/classic"}
-              className="hover:border-b-4 border-yellow-300 hover:font-semibold"
+              className={`${
+                selectedMenu === 4 && "font-bold border-b-4 border-yellow-300"
+              } hover:border-b-4 border-yellow-300 hover:font-semibold`}
             >
               클래식
             </Link>
